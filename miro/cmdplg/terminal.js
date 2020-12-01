f7673c4ce2b00387cead146f736cb5fa0d8b892d
mialso <mialsot@gmail.com>
2020-12-01 17:28:12 +0500

    first terminal inclusion

--- miro/cmdplg/terminal.js
+++ miro/cmdplg/terminal.js
@@ -1,5 +1,10 @@
+
 miro.onReady(function() {
     miro.broadcastData(terminalMounted())
+    debugger
+    const term = new Terminal();
+    term.open(document.getElementById('terminal'));
+    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
     return
 })
 
