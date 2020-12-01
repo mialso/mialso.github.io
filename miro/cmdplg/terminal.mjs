3937eed8434eef1f4b84788d4948da814399bbd9
mialso <mialsot@gmail.com>
2020-12-01 21:22:44 +0500

    make timeout to ensure load

--- miro/cmdplg/terminal.mjs
+++ miro/cmdplg/terminal.mjs
@@ -2,16 +2,18 @@ import { terminalMounted } from './action.mjs'
 
 document.addEventListener('DOMContentLoaded', () => {
     miro.onReady(() => {
-        miro.broadcastData(terminalMounted())
-        const term = new Terminal();
-        term.open(document.getElementById('terminal'));
-        term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-        term.on('key', (key, ev) => {
-            console.log(key.charCodeAt(0))
-            if (key.charCodeAt(0) === 13) {
-                term.write('\n')
-            }
-            term.write(key)
-        })
+        setTimeout(() => {
+            miro.broadcastData(terminalMounted())
+            const term = new Terminal();
+            term.open(document.getElementById('terminal'));
+            term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+            term.on('key', (key, ev) => {
+                console.log(key.charCodeAt(0))
+                if (key.charCodeAt(0) === 13) {
+                    term.write('\n')
+                }
+                term.write(key)
+            })
+        }, 1500)
     })
 })
