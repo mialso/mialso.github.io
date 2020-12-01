934c14e070e2d96c1f5351f0b763924745e20ccd
mialso <mialsot@gmail.com>
2020-12-01 21:18:18 +0500

    make use of domcontentloaded

--- miro/cmdplg/terminal.mjs
+++ miro/cmdplg/terminal.mjs
@@ -1,15 +1,17 @@
 import { terminalMounted } from './action.mjs'
 
-miro.onReady(() => {
-    miro.broadcastData(terminalMounted())
-    const term = new Terminal();
-    term.open(document.getElementById('terminal'));
-    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-    term.on('key', (key, ev) => {
-        console.log(key.charCodeAt(0))
-        if (key.charCodeAt(0) === 13) {
-            term.write('\n')
-        }
-        term.write(key)
+document.addEventListener('DOMContentLoaded', () => {
+    miro.onReady(() => {
+        miro.broadcastData(terminalMounted())
+        const term = new Terminal();
+        term.open(document.getElementById('terminal'));
+        term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+        term.on('key', (key, ev) => {
+            console.log(key.charCodeAt(0))
+            if (key.charCodeAt(0) === 13) {
+                term.write('\n')
+            }
+            term.write(key)
+        })
     })
 })
