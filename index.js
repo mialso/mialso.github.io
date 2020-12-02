462bb00cfa2f9feccf794ac321495f9eeabcc9ee
Anton Kislitsyn <anton.v.k@miro.com>
2020-12-02 16:01:10 +0100

    Changed to root level

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var app = express();
 app.use(express.static('public'));
 
 //Serves all the request which includes /images in the url from Images folder
-app.use('/', express.static(__dirname + '/miro/cmdplg/'));
+app.use('/', express.static(__dirname + '/'));
 
 var server = app.listen(process.env.PORT);
