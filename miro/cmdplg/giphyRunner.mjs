2a3d209a4a910f1f049b6c961718aa7905b16b20
Marcin Pajak <marcin@miro.com>
2020-12-02 14:03:06 +0100

    fix: use proper structure

--- miro/cmdplg/giphyRunner.mjs
+++ miro/cmdplg/giphyRunner.mjs
@@ -13,11 +13,11 @@ export const runGiphy = async (action) => {
 
         console.log('RESULTS', result);
 
-        if (!result.url) {
+        if (!result.data || !result.data.url) {
             return;
         }
 
-        const url = decodeURIComponent(result.image_original_url)
+        const url = decodeURIComponent(result.data.image_original_url)
         miro.board.widgets.images.createByURL(url);
     } catch (error) {
         // nothing for now
