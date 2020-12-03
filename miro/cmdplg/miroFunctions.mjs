af37a7d48ddeb4f6fca245e514ad1ca5cb69ce8e
Anton Kislitsyn <anton.v.k@miro.com>
2020-12-03 11:46:16 +0100

    Fixed createImage

--- miro/cmdplg/miroFunctions.mjs
+++ miro/cmdplg/miroFunctions.mjs
@@ -1,9 +1,9 @@
-export createImageByUrl = (url, tag) => {
+export const createImageByUrl = (url, tag) => {
     miro.board.viewport.get()
         .then(vp => {
             const targetX = (vp.x + vp.width) / 2;
             const targetY = (vp.y + vp.height) / 2;
-            const options = { x: targetX, y: targetY, title: action.payload };
+            const options = { x: targetX, y: targetY, title: tag };
             miro.board.widgets.images.createByURL(url, options)
                 .then(widget => miro.board.selection.selectWidgets(widget.id));
         })
